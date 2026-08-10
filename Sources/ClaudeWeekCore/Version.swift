46268ba6152303ef43385893fe7fb5370de18606
Greem4 <i@greem4.ru>
2026-08-10 10:04:14 +0400

    Поднял версию до 0.1.5

--- Sources/ClaudeWeekCore/Version.swift
+++ Sources/ClaudeWeekCore/Version.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum ClaudeWeek {
-    public static let version = "0.1.4"
+    public static let version = "0.1.5"
     public static let bundleIdentifier = "com.greem4.claudeweek"
     /// Откуда приходят обновления: `владелец/репозиторий` на GitHub. Тот же
     /// репозиторий выпускает образы, поэтому имя одно на проверку версии и на
